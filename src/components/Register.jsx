1c769bcc8d1ba8b328a99fc6dfe10f96884df823
kutukov2000 <kutukov16.2005@gmail.com>
2023-12-19 16:27:02 +0200

    refactor: clear Register component

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,36 +1,33 @@
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input } from "@nextui-org/react";
 import { MailIcon } from './Icons/MailIcon.jsx';
 import { LockIcon } from './Icons/LockIcon.jsx';
-import { login, logout, selectIsLoggined, selectToken } from "../store/userSlice.js";
+import { login, selectIsLoggined } from "../store/userSlice.js";
 import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import AccountService from "./Services/AccountService.js";
 
 export default function Register() {
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
-    const dispatch = useDispatch();
-    const { register, handleSubmit } = useForm();
-
-    const submitForm = async (data) => {
-
-        console.log("register");
-        if(await AccountService.Register(data)){
-            const token = await AccountService.Login(data);
-            const user = data;
-            
-            dispatch(login({ user, token }));
-        }
-
-        //else Toast
-
-    }
-
-    const token = useSelector(selectToken);
-    console.log('Token is: ', token);
 
     const isLogged = useSelector(selectIsLoggined);
-    console.log(isLogged);
+
+    //Modal window
+    const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const dispatch = useDispatch();
+
+    const submitForm = async (data) => {
+        
+        if (await AccountService.Register(data)) {
+            const token = await AccountService.Login(data);
+            const user = data;
+
+            dispatch(login({ user, token }));
+
+            onClose();
+        }
+    }
 
     return (
         <div>
@@ -42,43 +39,40 @@ export default function Register() {
                 onOpenChange={onOpenChange}
                 placement="top-center">
                 <ModalContent>
-                    {(onClose) => (
-                        <form onSubmit={handleSubmit(submitForm)}>
-                            <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
-                            <ModalBody>
-                                <Input
-                                    autoFocus
-                                    endContent={
-                                        <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                                    }
-                                    label="Email"
-                                    placeholder="Enter your email"
-                                    variant="bordered"
-                                    {...register('email')}
-                                    required
-                                />
-                                <Input
-                                    endContent={
-                                        <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                                    }
-                                    label="Password"
-                                    placeholder="Enter your password"
-                                    type="password"
-                                    variant="bordered"
-                                    {...register('password')}
-                                    required
-                                />
-                            </ModalBody>
-                            <ModalFooter>
-                                <Button color="danger" variant="flat" onPress={onClose}>
-                                    Close
-                                </Button>
-                                <Button type="submit" color="primary" onPress={onClose}>
-                                    Sign in
-                                </Button>
-                            </ModalFooter>
-                        </form>
-                    )}
+                    <form onSubmit={handleSubmit(submitForm)}>
+                        <ModalHeader className="flex flex-col gap-1 text-uppercase">Sign up</ModalHeader>
+                        <ModalBody>
+                            <Input
+                                autoFocus
+                                endContent={
+                                    <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                                }
+                                label="Email"
+                                variant="bordered"
+                                {...register('email')}
+                                required
+                            />
+                            {errors.title && <p style={{ color: '#cc4137' }}>{errors.title.message}</p>}
+                            <Input
+                                endContent={
+                                    <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                                }
+                                label="Password"
+                                type="password"
+                                variant="bordered"
+                                {...register('password')}
+                                required />
+                            {errors.title && <p style={{ color: '#cc4137' }}>{errors.title.message}</p>}
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="danger" variant="flat" onPress={onClose}>
+                                Close
+                            </Button>
+                            <Button type="submit" color="primary">
+                                Sign up
+                            </Button>
+                        </ModalFooter>
+                    </form>
                 </ModalContent>
             </Modal>
         </div>
