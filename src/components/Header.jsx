ce8643c8360a6c135c31b09f59c0aa671f637b3d
kutukov2000 <kutukov16.2005@gmail.com>
2023-12-19 15:51:36 +0200

    style: use NextUI NavBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import Menu from './Menu'
 import Account from './Account'
 import Register from './Register'
+import { Link } from 'react-router-dom';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
+
 export default function Header() {
-
-
     return (
-        <header>
-            <Menu />
-            <Account />
-            <Register/>
-        </header>
+        <Navbar>
+            <NavbarBrand>
+                <Link to="/">
+                    <p className="fw-bolder text-uppercase fs-5">Surveys</p>
+                </Link>
+            </NavbarBrand>
+            <NavbarContent justify="end" className='gap-2'>
+                <NavbarItem>
+                    <Account />
+                </NavbarItem>
+                <NavbarItem>
+                    <Register />
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
     )
 }
